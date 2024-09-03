b67a2be3935ec3a43f9c7df08f0f33b0c6c95334
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2024-09-03 15:57:53 +0200

    Sorted allauth templates css

--- residenza127/settings.py
+++ residenza127/settings.py
@@ -86,7 +86,10 @@ ROOT_URLCONF = 'residenza127.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -164,15 +167,15 @@ CLOUDINARY_STORAGE = {
 }
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = \
     'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
