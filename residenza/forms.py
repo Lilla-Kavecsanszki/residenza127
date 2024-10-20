75dfdfc5a1bacc32ab9898a99b3d412d256df6c1
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2024-10-20 16:44:40 +0200

    Added placeholders to contact form

--- residenza/forms.py
+++ residenza/forms.py
@@ -1,18 +1,20 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from phonenumber_field.formfields import PhoneNumberField
-
 from .models import Contact
-
 
 class ContactForm(forms.ModelForm):
     phone_number = PhoneNumberField(
         required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'es. +39 333 123 4567'})  # Italian placeholder example
+        widget=forms.TextInput(attrs={'placeholder': _('e.g., +39 333 123 4567')})
     )
 
     class Meta:
         model = Contact
         fields = ["name", "email", "phone_number", "subject", "message"]
         widgets = {
-            "message": forms.Textarea(attrs={"rows": 4}),
+            "name": forms.TextInput(attrs={'placeholder': _('John Doe')}),
+            "email": forms.EmailInput(attrs={'placeholder': _('example.site.com')}),
+            "subject": forms.TextInput(attrs={'placeholder': _('What would you like to discuss?')}),
+            "message": forms.Textarea(attrs={'rows': 4, 'placeholder': _('Your Message')}),
         }
